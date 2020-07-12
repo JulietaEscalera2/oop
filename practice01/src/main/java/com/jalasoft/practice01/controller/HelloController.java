a9133fab0f6d12dfd5cd720afef9a93c6e070817
LAPTOP-Q6VQ16OM\julie <julieta.escalera@gmail.com>
2020-07-12 00:53:47 -0400

    Add read and save document

--- practice01/src/main/java/com/jalasoft/practice01/controller/HelloController.java
+++ practice01/src/main/java/com/jalasoft/practice01/controller/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Julieta
@@ -19,10 +23,17 @@ public class HelloController {
         return "Hello Julieta";
     }
 
-    @PostMapping
+   // @PostMapping
     public String sayHello(@RequestParam (value = "name") String name,
                            @RequestParam(value = "lastName") String lastName){
         return "Hello "+ name + " " +lastName;
+    }
+    @PostMapping
+
+    public String getFile(@RequestParam(value = "file")MultipartFile file) throws IOException {
+        File destination = new File("C:\\Courses\\oop\\Documents");
+        file.transferTo(destination);
+        return "File: "+ file.getOriginalFilename();
     }
 
 
