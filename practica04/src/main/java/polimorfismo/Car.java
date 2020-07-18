ed28a6133e4d92528df39abb1a4ebbb99ce9c937
LAPTOP-Q6VQ16OM\julie <julieta.escalera@gmail.com>
2020-07-18 03:12:15 -0400

    Practica4 v02

--- practica04/src/main/java/polimorfismo/Car.java
+++ practica04/src/main/java/polimorfismo/Car.java
@@ -1,4 +1,20 @@
 package polimorfismo;
 
-public class Car {
+public class Car extends Land{
+    private boolean useGas;
+
+    public Car(String name, int price, boolean hasMotor, boolean useGas) {
+        super(name, price, hasMotor);
+        this.useGas = useGas;
+    }
+
+    public boolean isUseGas() {
+        return useGas;
+    }
+
+
+    @Override
+    public String displayData() {
+        return ", useGas = "+String.valueOf(isUseGas());
+    }
 }
--- practica04/src/main/java/polimorfismo/Main.java
+++ practica04/src/main/java/polimorfismo/Main.java
@@ -1,4 +1,23 @@
 package polimorfismo;
 
 public class Main {
+
+    public static void main (String [] args){
+
+        ListLandTransport trans = new ListLandTransport();
+
+        Land transport1 = new Bicycle("Cross",1500,false, true);
+        Land transport2 = new Bicycle("mongo",1500,false, false);
+        Land transport3 = new Car("Audi",15000,true, true);
+        Land transport4 = new Car("Toyota",25000,true, false);
+
+        trans.addLand(transport1);
+        trans.addLand(transport2);
+        trans.addLand(transport3);
+        trans.addLand(transport4);
+
+        trans.display();
+
+
+    }
 }
